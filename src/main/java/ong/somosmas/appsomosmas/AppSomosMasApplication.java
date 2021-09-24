56731d7d2b33b2e45e4015786939200356b55de7
caito <caitocd@gmail.com>
2021-09-24 12:14:17 -0300

    first commit

---
+++ src/main/java/ong/somosmas/appsomosmas/AppSomosMasApplication.java
@@ -0,0 +1,13 @@
+package ong.somosmas.appsomosmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSomosMasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSomosMasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ong/somosmas/appsomosmas/AppSomosMasApplicationTests.java
@@ -0,0 +1,13 @@
+package ong.somosmas.appsomosmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSomosMasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
